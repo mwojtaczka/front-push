0454ba2f87d2cd9d026c6996da7999520d44062d
mwojtacz <mwojtacz@akamai.com>
2022-03-06 10:35:42 +0100

    Init

---
+++ src/main/java/com/maciej/wojtaczka/frontpush/FrontPushApplication.java
@@ -0,0 +1,13 @@
+package com.maciej.wojtaczka.frontpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maciej/wojtaczka/frontpush/FrontPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciej.wojtaczka.frontpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
